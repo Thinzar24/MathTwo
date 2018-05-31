049f2fae1fc23c3fa9dc9b946551aff96c8c4c57
Thinzar <tinzar.myathein@gmail.com>
2018-05-31 15:29:51 -0400

    initial commit

---
+++ src/com/company/MathTwo.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class MathTwo {
+
+    public static void main(String[] args) {
+	// write your code here
+        int mynumber = 2;
+        double myothernumber= 1.7938;
+        System.out.println(" Total "+ mynumber );
+        System.out.println(" Result " + myothernumber);
+    }
+}
